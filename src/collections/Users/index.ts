614f5aaa67b1097cc8b7369d2a2c4dc65377f27e
Marcoparada17 <Marcoparada17@gmail.com>
2025-11-25 18:26:33 -0400

    fix: remove ecommerce joins from Users collection

--- src/collections/Users/index.ts
+++ src/collections/Users/index.ts
@@ -26,63 +26,28 @@ export const Users: CollectionConfig = {
     tokenExpiration: 1209600,
   },
   fields: [
-    {
-      name: 'name',
-      type: 'text',
+  {
+    name: 'name',
+    type: 'text',
+  },
+  {
+    name: 'roles',
+    type: 'select',
+    access: {
+      create: adminOnlyFieldAccess,
+      read: adminOnlyFieldAccess,
+      update: adminOnlyFieldAccess,
     },
-    {
-      name: 'roles',
-      type: 'select',
-      access: {
-        create: adminOnlyFieldAccess,
-        read: adminOnlyFieldAccess,
-        update: adminOnlyFieldAccess,
-      },
-      defaultValue: ['customer'],
-      hasMany: true,
-      hooks: {
-        beforeChange: [ensureFirstUserIsAdmin],
-      },
-      options: [
-        {
-          label: 'admin',
-          value: 'admin',
-        },
-        {
-          label: 'customer',
-          value: 'customer',
-        },
-      ],
+    defaultValue: ['customer'],
+    hasMany: true,
+    hooks: {
+      beforeChange: [ensureFirstUserIsAdmin],
     },
-    {
-      name: 'orders',
-      type: 'join',
-      collection: 'orders',
-      on: 'customer',
-      admin: {
-        allowCreate: false,
-        defaultColumns: ['id', 'createdAt', 'total', 'currency', 'items'],
-      },
-    },
-    {
-      name: 'cart',
-      type: 'join',
-      collection: 'carts',
-      on: 'customer',
-      admin: {
-        allowCreate: false,
-        defaultColumns: ['id', 'createdAt', 'total', 'currency', 'items'],
-      },
-    },
-    {
-      name: 'addresses',
-      type: 'join',
-      collection: 'addresses',
-      on: 'customer',
-      admin: {
-        allowCreate: false,
-        defaultColumns: ['id'],
-      },
-    },
-  ],
+    options: [
+      { label: 'admin', value: 'admin' },
+      { label: 'customer', value: 'customer' },
+    ],
+  },
+]
+
 }
